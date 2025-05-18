d320e542f8b2f1e6990a8565b5e4aa9b29cb0bce
FeralAI <13342258+FeralAI@users.noreply.github.com>
2025-05-18 14:49:06 -0500

    Upgrade JamesDSP Magisk module to v6.4

--- app/src/main/assets/app/support/subscripts/jdsp.magisk.sh
+++ app/src/main/assets/app/support/subscripts/jdsp.magisk.sh
@@ -7,7 +7,7 @@ LOG_FILE="$WORKING_PATH/jdsp.magisk.log"
 
 echo "Install JamesDSP Magisk Module started" > $LOG_FILE
 
-cp -fv $APPFILES_PATH/app/support/magisk/ainur_jamesdsp-v6.1-trimmed.zip $DOWNLOAD_PATH/ >> $LOG_FILE
-magisk --install-module $DOWNLOAD_PATH/ainur_jamesdsp-v6.1-trimmed.zip >> $LOG_FILE
+cp -fv $APPFILES_PATH/app/support/magisk/jdsp_v6.4-trimmed.zip $DOWNLOAD_PATH/ >> $LOG_FILE
+magisk --install-module $DOWNLOAD_PATH/jdsp_v6.4-trimmed.zip >> $LOG_FILE
 
 echo "Install JamesDSP Magisk Module finished" >> $LOG_FILE
