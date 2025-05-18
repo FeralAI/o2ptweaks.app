217f8f678486525d76a68615a487db09a18d29d7
FeralAI <13342258+FeralAI@users.noreply.github.com>
2025-05-18 14:55:29 -0500

    Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.feralai.o2ptweaks"
         minSdk = 29
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.2"
+        versionCode = 3
+        versionName = "0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
